77147c500f65d54599e1eebd636357e0198bb61b
Ryan Qin <qinang5543@gmail.com>
2020-05-03 14:05:24 -0400

    websocket and more

--- frontend/src/components/ChatList.vue
+++ frontend/src/components/ChatList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chat-list">
-    <h>This is your chats</h>
+    <h2>This is your chats</h2>
     <b-list-group v-for="(chat, id) in chats" :key="id">
       <b-list-group-item>{{ chat.from }}</b-list-group-item>
       <b-list-group-item>{{ chat.message }}</b-list-group-item>
@@ -12,26 +12,19 @@
 import { Component, Model, Vue } from "vue-property-decorator";
 import gql from "graphql-tag";
 import ChatModel from "../model/chat";
+import { QUERY_CHATS } from "../graphql/chat.graphql";
 
 @Component({
   apollo: {
     chats() {
       return {
-        query: gql`
-          query {
-            chats {
-              id
-              message
-              from
-            }
-          }
-        `,
+        query: QUERY_CHATS,
         result(result: any) {
           console.log(result);
-        }
+        },
       };
-    }
-  }
+    },
+  },
 })
 export default class ChatList extends Vue {
   chats: ChatModel[] = [];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
+import { split } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import Vue from "vue";
@@ -11,14 +14,32 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-const link = new HttpLink({
+const httpLink = new HttpLink({
   uri: `http://localhost:4000`,
 });
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000",
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query) as any;
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
 const apolloClient = new ApolloClient({
   link,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
+
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 });
